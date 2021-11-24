fd3f719ee98796c0bcaa1490127b4dba0c604b22
Anamaria Camarasan <acamaras@c2r5p9.hive.fi>
2021-11-24 13:28:34 +0200

    added author file

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -13,7 +13,7 @@
 #include <string.h>
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*d;
